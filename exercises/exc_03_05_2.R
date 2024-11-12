a2de46a844b5d5a74bcbe64b06fe0b27acdcf4f6
Matthew Krause <matthew.krause@mcgill.ca>
2024-11-12 15:47:49 -0500

    Update exc_03_05_2.R
    Removed accidental printing of `s` function

--- exercises/exc_03_05_2.R
+++ exercises/exc_03_05_2.R
@@ -2,6 +2,6 @@ library(mgcv)
 data(meuse, package="sp")
 mod2da <- gam(cadmium ~ s(x, y) + s(dist) + s(elev),
               data = meuse, method = "REML")
-s
+
 # Colored heat map
 plot(mod2da, ___, ___)
